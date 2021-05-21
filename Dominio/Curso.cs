7bffdd93c4125730306a28cdbaed162aabd0ada0
teresavargas19 <vargasbrito19@gmail.com>
2021-05-20 23:09:22 -0400

    Creacion de clases en dominio

---
+++ Dominio/Curso.cs
@@ -0,0 +1,11 @@
+namespace Dominio
+{
+    public class Curso
+    {
+     public int CursoId {get; set;}
+     public string Titulo {get; set;}
+     public string Descripcion {get;set;}
+     public DataTime FechaPublicacion {get;set;}
+     public byte[] FotoPortada {get;set;}   
+    }
+}
